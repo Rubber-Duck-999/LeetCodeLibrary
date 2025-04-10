4c32b62ae2a8af77efc4d558d45bd4e1e11aafe0
rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2025-04-10 18:29:40 +0900

    Simplify

--- src/TwoSum.cc
+++ src/TwoSum.cc
@@ -1,19 +1,13 @@
 #include <TwoSum.h>
 
 std::vector<int> TwoSum::Calculate(std::vector<int>& nums, int target) {
-    std::unordered_map<int, int> num_map; 
-    // Stores {number, index}
-
-    for (std::size_t i = 0; i < nums.size(); i++) {
-        // Loop through vector
+    std::unordered_map<int, int> map;  // value -> index
+    for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
         int complement = target - nums[i];
-        if (num_map.find(complement) != num_map.end()) {
-            return {static_cast<int>(num_map[complement]), static_cast<int>(i)}; 
-            // Found the pair
+        if (map.count(complement)) {
+            return {map[complement], i};
         }
-        num_map[nums[i]] = i; 
-        // Store the index of the current number
+        map[nums[i]] = i;
     }
-    return {};
-    // Should never reach here as per problem constraints
+    return {}; // Not found
 }
